82e4be9c3ae7157b794954c1aa1c9b9c0cc3a9c0
anthony.buchholz <anthony.buchholz@gmail.com>
2019-02-24 00:38:36 -0600

    Adding constants file that wasn't added.

---
+++ quiplash-gamemaster-angular/src/app/constants/constants.ts
@@ -0,0 +1,9 @@
+export const constants = {
+    voteTimeout: 20,
+    voteResultsCountdown: 5,
+    answerPhase: 'answering questions',
+    joinPhase: 'joining',
+    votePhase: 'voting',
+    waitPhase: 'waiting',
+    finalPhase: 'final results'
+}
